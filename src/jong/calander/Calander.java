fc453bd017c0b67bdcb211e8ead5cc58ae631011
Administrator <Administrator@114.200.252.174>
2017-12-30 20:35:38 +0900

    Fix Endcoding Problem

--- src/jong/calander/Calander.java
+++ src/jong/calander/Calander.java
@@ -1,10 +1,25 @@
 package jong.calander;
 
+import java.util.Scanner;
+
 public class Calander {
 
 	public static void main(String[] args) {
-		// TODO ÀÚµ¿ »ı¼ºµÈ ¸Ş¼Òµå ½ºÅÓ
-		System.out.println("Hello, Calander");
+		// TODO ìë™ ìƒì„±ëœ ë©”ì†Œë“œ ìŠ¤í…
+		
+		System.out.println("ì¼ ì›”  í™”  ìˆ˜  ëª©  ê¸ˆ  í† ");
+		System.out.println("1   2   3   4    5   6   7");
+		System.out.println("8   9  10  11  12  13  14");
+		System.out.println("15 16 17  18  19  20  21");
+		System.out.println("22 23 24  25  26  27  28");
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }

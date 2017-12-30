96172e969a48a119304669dfd8dcc6d2ca439dcf
Administrator <Administrator@114.200.252.174>
2017-12-30 20:41:02 +0900

    Example Sum

---
+++ src/jong/calander/Sum.java
@@ -0,0 +1,27 @@
+package jong.calander;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+
+		int a, b;
+
+		Scanner scanner = new Scanner(System.in);
+
+		String a1, b1;
+
+		a1 = scanner.next();
+		b1 = scanner.next();
+
+		a = Integer.parseInt(a1);
+		b = Integer.parseInt(b1);
+		
+		
+		System.out.println(a+b);
+		
+		
+
+	}
+}

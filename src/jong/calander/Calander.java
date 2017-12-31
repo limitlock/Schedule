9c96046b400a24daff8f49de4a44e8693711b286
Administrator <Administrator@210.205.54.178>
2017-12-31 19:19:22 +0900

    Find(Auto without weekday) Calender

--- src/jong/calander/Calander.java
+++ src/jong/calander/Calander.java
@@ -2,8 +2,8 @@ package jong.calander;
 
 public class Calander {
 
-	private static final int[] END_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final int[] LEAP_END_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] END_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_END_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
@@ -15,30 +15,70 @@ public class Calander {
 
 	public int getEndOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
-			return LEAP_END_DAYS[month - 1];
+			return LEAP_END_DAYS[month];
 		} else {
-			return END_DAYS[month - 1];
+			return END_DAYS[month];
 		}
 
 	}
 
 	public void printCalander(int year, int month) {
-		System.out.printf("    <<%4d년%3d월>> \n", year, month);
-		System.out.println("  일 월  화 수 목  금  토");
+		System.out.printf("  <<%d년%d월>> \n", year, month);
+		System.out.println(" SU MO TU WD TH FR SA");
 		System.out.println(" --------------------");
 
-		int endDay = getEndOfMonth(year, month);
+		// get weekday auto
+		int weekday = getWeekDay(year, month, 1);
 
-		for (int i = 1; i <= endDay; i++) {
+		// print blank
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
+		int endDay = getEndOfMonth(year, month);
+		int count = 7 - weekday;
+		int div_condition = (count < 7) ? count : 0;
+
+		// print first line
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		for (int i = count + 1; i <= endDay; i++) {
 
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+			if (i % 7 == div_condition) {
 				System.out.println();
 			}
 
 		}
-		System.out.println("");
+		System.out.println();
+		System.out.println();
 
+	}
+
+	// 1970년 1월 1일 목요일을 기준으로 요일을 구한다.
+	private int getWeekDay(int year, int month, int day) {
+		// TODO 자동 생성된 메소드 스텁
+		int f_year = 1970;
+		final int F_WEEKDAY = 3;
+
+		int count = 0;
+		for (int i = f_year; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+
+		for (int i = 1; i < month; i++) {
+			int delta = getEndOfMonth(year, i);
+			count += delta;
+		}
+
+		count += day;
+
+		int weekday = (count + F_WEEKDAY) % 7;
+		return weekday;
 	}
 
 }
